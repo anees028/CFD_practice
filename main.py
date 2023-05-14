861ce8209d37efe02c324c956161ae1c9fd6ee0e
anees028 <rehmananees028@gmail.com>
2023-05-14 14:45:12 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as pl
+
+x, dx = np.linspace(0.,1.,11, retstep=True)
+
+
+print("X  = ",x)
+print("First Element : ",x[0])
+print("Second Element : ",x[1])
+print("Last Element : ",x[-1])
+
+
+
+# y = 1. -2.*x
+#
+# print("Result of Function = ", y)
+#
+# pl.plot(x,y)
+# pl.show()
+
+
+
+
